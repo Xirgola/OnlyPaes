2b11ebad29d912dcc512a5695eee9e71e5033fcd
Igor Nery <igornery135@gmail.com>
2025-09-10 22:13:12 -0300

    Comandas
    foi implementado a aba de comandas com txbComanda, txbProduto e txbQuantidade, com botão continuar, lançar e cancelar com todos eles com suas respectivas funções funcionais

--- OnlyPaes/FormComandas.Designer.cs
+++ OnlyPaes/FormComandas.Designer.cs
@@ -28,12 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormComandas));
+            this.dgvProdutos = new System.Windows.Forms.DataGridView();
+            this.txbCodProdutoInfo = new System.Windows.Forms.TextBox();
+            this.lblCodProdInfo = new System.Windows.Forms.Label();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.lblComandaInfo = new System.Windows.Forms.Label();
+            this.txbComandaInfo = new System.Windows.Forms.TextBox();
+            this.grbInfo = new System.Windows.Forms.GroupBox();
+            this.grbLancamento = new System.Windows.Forms.GroupBox();
+            this.txbProduto = new System.Windows.Forms.TextBox();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.btnLancar = new System.Windows.Forms.Button();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.txbQuantidade = new System.Windows.Forms.TextBox();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).BeginInit();
+            this.grbInfo.SuspendLayout();
+            this.grbLancamento.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProdutos
+            // 
+            this.dgvProdutos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdutos.Location = new System.Drawing.Point(28, 115);
+            this.dgvProdutos.Name = "dgvProdutos";
+            this.dgvProdutos.Size = new System.Drawing.Size(573, 452);
+            this.dgvProdutos.TabIndex = 5;
+            this.dgvProdutos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProdutos_CellClick);
+            // 
+            // txbCodProdutoInfo
+            // 
+            this.txbCodProdutoInfo.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbCodProdutoInfo.Location = new System.Drawing.Point(172, 98);
+            this.txbCodProdutoInfo.Name = "txbCodProdutoInfo";
+            this.txbCodProdutoInfo.Size = new System.Drawing.Size(138, 39);
+            this.txbCodProdutoInfo.TabIndex = 3;
+            // 
+            // lblCodProdInfo
+            // 
+            this.lblCodProdInfo.AutoSize = true;
+            this.lblCodProdInfo.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodProdInfo.Location = new System.Drawing.Point(31, 108);
+            this.lblCodProdInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCodProdInfo.Name = "lblCodProdInfo";
+            this.lblCodProdInfo.Size = new System.Drawing.Size(132, 20);
+            this.lblCodProdInfo.TabIndex = 2;
+            this.lblCodProdInfo.Text = "Cod, Produto:";
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinuar.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinuar.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnContinuar.Location = new System.Drawing.Point(71, 169);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(207, 39);
+            this.btnContinuar.TabIndex = 4;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // lblComandaInfo
+            // 
+            this.lblComandaInfo.AutoSize = true;
+            this.lblComandaInfo.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComandaInfo.Location = new System.Drawing.Point(67, 38);
+            this.lblComandaInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblComandaInfo.Name = "lblComandaInfo";
+            this.lblComandaInfo.Size = new System.Drawing.Size(96, 20);
+            this.lblComandaInfo.TabIndex = 0;
+            this.lblComandaInfo.Text = "Comanda:";
+            // 
+            // txbComandaInfo
+            // 
+            this.txbComandaInfo.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbComandaInfo.Location = new System.Drawing.Point(172, 19);
+            this.txbComandaInfo.Name = "txbComandaInfo";
+            this.txbComandaInfo.Size = new System.Drawing.Size(138, 39);
+            this.txbComandaInfo.TabIndex = 1;
+            // 
+            // grbInfo
+            // 
+            this.grbInfo.Controls.Add(this.txbComandaInfo);
+            this.grbInfo.Controls.Add(this.lblComandaInfo);
+            this.grbInfo.Controls.Add(this.btnContinuar);
+            this.grbInfo.Controls.Add(this.lblCodProdInfo);
+            this.grbInfo.Controls.Add(this.txbCodProdutoInfo);
+            this.grbInfo.Location = new System.Drawing.Point(607, 130);
+            this.grbInfo.Name = "grbInfo";
+            this.grbInfo.Size = new System.Drawing.Size(316, 223);
+            this.grbInfo.TabIndex = 6;
+            this.grbInfo.TabStop = false;
+            this.grbInfo.Text = "Informações";
+            // 
+            // grbLancamento
+            // 
+            this.grbLancamento.Controls.Add(this.txbProduto);
+            this.grbLancamento.Controls.Add(this.lblProduto);
+            this.grbLancamento.Controls.Add(this.btnLancar);
+            this.grbLancamento.Controls.Add(this.lblQuantidade);
+            this.grbLancamento.Controls.Add(this.txbQuantidade);
+            this.grbLancamento.Enabled = false;
+            this.grbLancamento.Location = new System.Drawing.Point(607, 359);
+            this.grbLancamento.Name = "grbLancamento";
+            this.grbLancamento.Size = new System.Drawing.Size(316, 223);
+            this.grbLancamento.TabIndex = 7;
+            this.grbLancamento.TabStop = false;
+            this.grbLancamento.Text = "Lançamento";
+            // 
+            // txbProduto
+            // 
+            this.txbProduto.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbProduto.Location = new System.Drawing.Point(172, 28);
+            this.txbProduto.Name = "txbProduto";
+            this.txbProduto.ReadOnly = true;
+            this.txbProduto.Size = new System.Drawing.Size(138, 39);
+            this.txbProduto.TabIndex = 1;
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduto.Location = new System.Drawing.Point(67, 38);
+            this.lblProduto.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(84, 20);
+            this.lblProduto.TabIndex = 0;
+            this.lblProduto.Text = "Produto:";
+            // 
+            // btnLancar
+            // 
+            this.btnLancar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancar.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancar.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btnLancar.Location = new System.Drawing.Point(71, 143);
+            this.btnLancar.Name = "btnLancar";
+            this.btnLancar.Size = new System.Drawing.Size(207, 39);
+            this.btnLancar.TabIndex = 4;
+            this.btnLancar.Text = "Lançar";
+            this.btnLancar.UseVisualStyleBackColor = true;
+            this.btnLancar.Click += new System.EventHandler(this.btnLancar_Click);
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantidade.Location = new System.Drawing.Point(31, 108);
+            this.lblQuantidade.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(113, 20);
+            this.lblQuantidade.TabIndex = 2;
+            this.lblQuantidade.Text = "Quantidade:";
+            // 
+            // txbQuantidade
+            // 
+            this.txbQuantidade.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbQuantidade.Location = new System.Drawing.Point(172, 98);
+            this.txbQuantidade.Name = "txbQuantidade";
+            this.txbQuantidade.Size = new System.Drawing.Size(138, 39);
+            this.txbQuantidade.TabIndex = 3;
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTexto.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(21, 41);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(403, 42);
+            this.lblTexto.TabIndex = 8;
+            this.lblTexto.Text = "Lançamento de comandas";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OnlyPaes.Properties.Resources.ChatGPT_Image_27_de_ago__de_2025__20_30_36;
+            this.pictureBox1.Location = new System.Drawing.Point(766, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.Red;
+            this.btnCancelar.Location = new System.Drawing.Point(678, 547);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(207, 29);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FormComandas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(217)))), ((int)(((byte)(198)))));
+            this.ClientSize = new System.Drawing.Size(961, 623);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.grbLancamento);
+            this.Controls.Add(this.grbInfo);
+            this.Controls.Add(this.dgvProdutos);
+            this.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.Name = "FormComandas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormComandas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdutos)).EndInit();
+            this.grbInfo.ResumeLayout(false);
+            this.grbInfo.PerformLayout();
+            this.grbLancamento.ResumeLayout(false);
+            this.grbLancamento.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dgvProdutos;
+        private System.Windows.Forms.TextBox txbCodProdutoInfo;
+        private System.Windows.Forms.Label lblCodProdInfo;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.Label lblComandaInfo;
+        private System.Windows.Forms.TextBox txbComandaInfo;
+        private System.Windows.Forms.GroupBox grbInfo;
+        private System.Windows.Forms.GroupBox grbLancamento;
+        private System.Windows.Forms.TextBox txbProduto;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.Button btnLancar;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.TextBox txbQuantidade;
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
--- OnlyPaes/FormComandas.cs
+++ OnlyPaes/FormComandas.cs
@@ -17,6 +17,94 @@ namespace OnlyPaes
         {
             InitializeComponent();
             this.usuario = usuario;
+            AtualizarDgv();
+        }
+        public void AtualizarDgv()
+        {
+            Model.Produto produto = new Model.Produto();
+            dgvProdutos.DataSource = produto.Listar();
+        }
+
+        private void dgvProdutos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Pegar a linha selecionada:
+            int ls = dgvProdutos.SelectedCells[0].RowIndex;
+
+            // Colocar o Id do produto no campo de codigo
+            txbCodProdutoInfo.Text = dgvProdutos.Rows[ls].Cells[0].Value.ToString();
+            // Colocar o nome do produto no campo de informações
+            txbProduto.Text = dgvProdutos.Rows[ls].Cells[1].Value.ToString();
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            // Verificar se os campos estao vazios:
+            if (txbComandaInfo.Text.Length == 0)
+            {
+                MessageBox.Show("Informe o número da comanda!", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txbCodProdutoInfo.Text.Length == 0)
+            {
+                MessageBox.Show("informe o codigo do produto!", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // Desativar o grbInfos:
+                grbInfo.Enabled = false;
+                // Ativar o grbLançamento
+                grbLancamento.Enabled = true;
+
+            }
+
+        }
+
+        private void btnLancar_Click(object sender, EventArgs e)
+        {
+            // Verificar se os campos estao vazios:
+            if (txbComandaInfo.Text.Length == 0)
+            {
+                MessageBox.Show("Informe a quantidade!", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+            else
+            {
+                Model.OrdemComanda ordemComanda = new Model.OrdemComanda();
+                ordemComanda.IdFicha = int.Parse(txbComandaInfo.Text);
+                ordemComanda.IdProduto = int.Parse(txbCodProdutoInfo.Text);
+                ordemComanda.Quantidade = int.Parse(txbQuantidade.Text);
+                ordemComanda.IdResp = usuario.Id;
+
+                if (ordemComanda.Cadastrar())
+                {
+                    MessageBox.Show("Lançamento efetuado", "Sucesso",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ResetarCampos();
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao efetuar o lançamento!", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void ResetarCampos()
+        {
+            txbCodProdutoInfo.Clear();
+            txbQuantidade.Clear();
+            txbProduto.Clear();
+            txbComandaInfo.Clear();
+            //Resetar os groupBox:
+            grbLancamento.Enabled = false;
+            grbInfo.Enabled = false;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            ResetarCampos();
         }
     }
 }
+    
